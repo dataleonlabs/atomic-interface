aec967d9f57dd11ee0ed0675bbcbaded9c7eeed6
Bhavin Karad <bhavinkarad@Bhavins-MacBook-Pro.local>
2019-08-06 18:18:28 +0530

    Create Spinner component

---
+++ src/display/Spinner/index.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Spinner from './index';
+import { Col } from 'reactstrap';
+
+storiesOf('Display|Spinner', module)
+  .add('Example', () => (
+    <React.Fragment>
+      <Col sm={4} style={{ marginTop: 30, marginLeft: 30 }}>
+        <h4>Spinner</h4>
+        <Spinner type="grow" color="primary" />
+        <Spinner type="grow" color="secondary" />
+        <Spinner type="grow" color="success" />
+        <Spinner type="grow" color="danger" />
+        <Spinner type="grow" color="warning" />
+        <Spinner type="grow" color="info" />
+        <Spinner type="grow" color="light" />
+        <Spinner type="grow" color="dark" />
+      </Col>
+    </React.Fragment>
+  ))
+ 
