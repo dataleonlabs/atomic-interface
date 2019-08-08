a219a696bde47d090c40d45735b2c165b11c1431
Bhavin Karad <bhavinkarad@Bhavins-MacBook-Pro.local>
2019-08-08 12:45:40 +0530

    bug fixed in HelpIcon

--- src/display/HelpIcon/index.tsx
+++ src/display/HelpIcon/index.tsx
@@ -3,13 +3,26 @@ import { HelpIconProps as Props } from './props'
 import { HelpCircle } from 'react-feather';
 import Popover from './../Popover'
 
-const HelpIcon = (props: Props) => (
-  <span>
-    <HelpCircle id="help-icon" size={17} />
-    <Popover {...props} target="help-icon" >
-      {props.children}
-    </Popover>
-  </span>
-);
+function makeId(length: number = 5) {
+  var result = 'y';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+const HelpIcon = (props: Props) => {
+  const target = `help-icon${makeId(2)}`;
+  return (
+    <span>
+      <HelpCircle id={target} size={17} />
+      <Popover {...props} target={target} >
+        {props.children}
+      </Popover>
+    </span>
+  );
+}
 
 export default HelpIcon;
