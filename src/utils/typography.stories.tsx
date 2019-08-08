b4a0b79fca4be5ae1840b76209e076d46c5e2d67
Pooja <pooja.p@samcomtechnobrains.com>
2019-08-08 13:11:47 +0530

    create util folder and typography files inside

---
+++ src/utils/typography.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+// import  {Styles}  from './typography'
+
+storiesOf('Utils', module)
+  .add('Example', () => (
+    <React.Fragment>
+   
+        <h4>Hello</h4>
+         
+     
+    </React.Fragment>
+ ));
