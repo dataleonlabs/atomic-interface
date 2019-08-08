1b54b7e2e05e21bad8b417ba5b9a9068a5be536c
Bhavin Karad <bhavinkarad@Bhavins-MacBook-Pro.local>
2019-08-08 21:43:53 +0530

    apply themeprovider for themeing component

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,27 @@
 // .storybook/config.ts
-import { configure } from '@storybook/react';
+import React from 'react'
+import { configure, addDecorator }from '@storybook/react';
 const req = require.context('../src', true, /\.stories\.tsx$/);
 
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { GlobalStyle } from "../src/utils/ThemeProvider"
+
+
+
+function withGlobalStyles(storyFn) {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      {storyFn()}
+    </React.Fragment>
+  );
+}
 
 configure(() => {
   req.keys().forEach(filename => req(filename));
 }, module);
+
+addDecorator(withGlobalStyles);
+
+
