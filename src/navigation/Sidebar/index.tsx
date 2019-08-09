afbe491a645ead59b0e538d9fde5124e11716048
samcom <rushabhsamcom84@gmail.com>
2019-08-09 12:31:56 +0530

    started implementing Sidebar

---
+++ src/navigation/Sidebar/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { SidebarProps as Props } from './props';
+import {Navbar, NavItem,Nav,Input,Button,NavbarBrand,NavLink} from 'reactstrap';
+
+/**
+ * Sidebar render element
+ */
+const Sidebar = (props: Props) => (
+  <div>
+   
+    <SidebarNav/>
+    
+  </div>
+  
+
+)
+const SidebarNav = (props: Props) => (
+  <div>
+   
+   <Nav vertical>
+        <SidebarNavItem/>
+    </Nav>
+    
+  </div>
+
+)
+const SidebarNavItem = (props: Props) => (
+  <div>
+          <NavItem>
+            <SidebarNavLink/>
+          </NavItem>
+   
+  </div>
+
+)
+const SidebarNavLink= (props: Props) => (
+  <div>
+    <NavLink href="#">Link</NavLink>  
+  </div>
+
+)
+
+export default Sidebar;
