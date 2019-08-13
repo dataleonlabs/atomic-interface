390ee7aaafa1659e494f1e71ab96415e27d61cca
Gérard TOKO <gerard@youngapp.co>
2019-08-13 09:25:41 +0200

    feat: add new deal architecture in ws cognito

--- src/utils/AuthProvider/AuthInterface.ts
+++ src/utils/AuthProvider/AuthInterface.ts
@@ -3,13 +3,6 @@
  * Follow documentation https://aws-amplify.github.io/docs/js/authentication#sign-in
  */
 
-export interface completeNewPassword {
-    user: any, // the User Object
-    newPassword: string, // the new password
-    meta?: { [key: string]: any }, // OPTIONAL, the required attributes
-    mfaType?: 'SMS_MFA' | 'SOFTWARE_TOKEN_MFA' // MFA Type e.g. SMS_MFA, SOFTWARE_TOKEN_MFA
-}
-
 export default interface AuthInterface {
 
     /** Configure object */
@@ -20,28 +13,33 @@ export default interface AuthInterface {
      * you will either sign in directly or be asked to pass some challenges
      * before getting authenticated.
      */
-    signIn: (email: string, password: string) => false | object;
+    signIn: (p: { email: string, password: string }) => false | object;
 
     /** 
      * If MFA is enabled, sign-in should be confirmed with the confirmation code
      */
-    confirmSignIn: (
+    confirmSignIn: (p: {
         user: any, // Return object from Auth.signIn()
         newPassword: string, // the new password
         meta?: { [key: string]: any }, // OPTIONAL, the required attributes
         mfaType?: 'SMS_MFA' | 'SOFTWARE_TOKEN_MFA' // MFA Type e.g. SMS_MFA, SOFTWARE_TOKEN_MFA
-    ) => false | object;
+    }) => false | object;
 
     /**
      * You need to get the new password and required attributes from the UI inputs
      * and then trigger the following function with a button click
      * For example, the email and phone_number are required attributes
      */
-    completeNewPassword: (p: completeNewPassword) => false | object;
+    completeNewPassword: (p: {
+        user: any, // the User Object
+        newPassword: string, // the new password
+        meta?: { [key: string]: any }, // OPTIONAL, the required attributes
+        mfaType?: 'SMS_MFA' | 'SOFTWARE_TOKEN_MFA' // MFA Type e.g. SMS_MFA, SOFTWARE_TOKEN_MFA
+    }) => false | object;
 
     /**
      * logout function
      * follow https://aws-amplify.github.io/docs/js/authentication#sign-out
      */
-    logout: (global?: boolean) => boolean;
+    logout: (p: { global?: boolean }) => boolean;
 }
