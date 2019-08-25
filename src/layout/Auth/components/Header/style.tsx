e6dfd5a5f996af3d5fb56dd16e59f3a2e3268e6d
Gérard TOKO <gerard@youngapp.co>
2019-08-25 09:31:16 +0200

    chore: merge from develop

---
+++ src/layout/Auth/components/Header/style.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import { CardHeader, CardTitle, CardSubtitle } from 'reactstrap';
+
+
+export const StyledCardHeader = styled(CardHeader)`
+  background-color: transparent !important;
+  border-bottom: none;
+`;
+
+export const StyledCardTitle = styled(CardTitle)`
+  text-align: center;
+  font-size: 24px;
+`;
+
+export const StyledCardSubTitle = styled(CardSubtitle)`
+  text-align: center;
+`;
