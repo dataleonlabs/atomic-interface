27e0707db1572ac1d8a9c4c07689cdcc3cfa4e0b
Reema Samcom <reema@samcomtechnobrains.com>
2019-08-31 13:09:29 +0530

    bug: added links to the introduction page AI-58

--- src/display/Introduction/index.stories.tsx
+++ src/display/Introduction/index.stories.tsx
@@ -20,7 +20,7 @@ storiesOf('Introduction|Introduction', module)
       <Col sm={8} style={{ marginTop: 30, marginLeft: 30 }}>
         <Breadcrumb>
             <BreadcrumbItem>Introduction</BreadcrumbItem>
-            <BreadcrumbItem><a href="#">Basic</a></BreadcrumbItem>
+            <BreadcrumbItem><a href="/?path=/story/introduction-introduction--basic">Basic</a></BreadcrumbItem>
         </Breadcrumb>
         <br/>
         <h1>Atomic Interface</h1>
@@ -38,7 +38,7 @@ storiesOf('Introduction|Introduction', module)
               <CardTitle><h6><strong>UI Elements</strong></h6></CardTitle>
               <br/>
               <CardText>UI Elements are those elements that can be found in any page with a single function and that can exist alone.</CardText>
-              <CardLink href="#">View Elements <ArrowRight size={14} /></CardLink>
+              <CardLink href="/?path=/story/ui-elements-abbreviation--basic-example">View Elements <ArrowRight size={14} /></CardLink>
             </CardBody>
           </Card>
           <Card>
@@ -49,7 +49,7 @@ storiesOf('Introduction|Introduction', module)
               <CardTitle><h6><strong>Forms</strong></h6></CardTitle>
               <br/>
               <CardText>Examples and usage guidelines for form control styles, layout options, and custom components...</CardText>
-              <CardLink href="#">View Forms <ArrowRight size={14} /></CardLink>
+              <CardLink href="/?path=/story/forms-form--form-layout">View Forms <ArrowRight size={14} /></CardLink>
             </CardBody>
           </Card>
         </CardDeck>
@@ -64,7 +64,7 @@ storiesOf('Introduction|Introduction', module)
               <CardTitle><h6><strong>Layouts</strong></h6></CardTitle>
               <br/>
               <CardText>Use Bootstrap's powerful mobile-first flexbox grid to build layouts of all shapes and sizes.</CardText>
-              <CardLink href="#">View Layouts <ArrowRight size={14} /></CardLink>
+              <CardLink href="/?path=/story/layout-forgot-password--example">View Layouts <ArrowRight size={14} /></CardLink>
             </CardBody>
           </Card>
           <Card>
@@ -75,7 +75,7 @@ storiesOf('Introduction|Introduction', module)
               <CardTitle><h6><strong>Navigation</strong></h6></CardTitle>
               <br/>
               <CardText>Responsive navigation header, the navbar. Includes support for branding, navigation, and more.</CardText>
-              <CardLink href="#">View Navigation <ArrowRight size={14} /></CardLink>
+              <CardLink href="/?path=/story/navigation-breadcrumb--basic-example">View Navigation <ArrowRight size={14} /></CardLink>
             </CardBody>
           </Card>
         </CardDeck>
@@ -90,7 +90,7 @@ storiesOf('Introduction|Introduction', module)
               <CardTitle><h6><strong>Get Started</strong></h6></CardTitle>
               <br/>
               <CardText>Get started with over a dozen reusable components built on top of Bootstrap with styles enhancement.</CardText>
-              <CardLink href="">View Introduction <ArrowRight size={14} /></CardLink>
+              <CardLink href="/?path=/story/get-started-typography--typography">View Introduction <ArrowRight size={14} /></CardLink>
             </CardBody>
           </Card>
           <Card>
@@ -101,7 +101,7 @@ storiesOf('Introduction|Introduction', module)
               <CardTitle><h6><strong>Others</strong></h6></CardTitle>
               <br/>
               <CardText>For faster mobile-friendly and responsive development, template includes dozens of utility...</CardText>
-              <CardLink href="#">View Utilities <ArrowRight size={14} /></CardLink>
+              <CardLink href="/?path=/story/ui-elements-abbreviation--basic-example">View Utilities <ArrowRight size={14} /></CardLink>
             </CardBody>
           </Card>
         </CardDeck>
