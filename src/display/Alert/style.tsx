18afd3c54098f9763ba132bb26f40375be2730a9
Reema Samcom <reema@samcomtechnobrains.com>
2019-09-02 19:29:45 +0530

    fix: alert styling fixed AI-80

--- src/display/Alert/style.tsx
+++ src/display/Alert/style.tsx
@@ -5,11 +5,29 @@ export const StyledAlert = styled.div`
   .alert {
       border: none;
       border-radius: 2px;
+      position:relative;
+      padding:0;
 
       .Text {
           vertical-align: top;
+          padding:10px 12px;
+          display:inline-block;
       }
-
+      
+      .alertIcon {
+        position: relative;
+        left: 0;
+        width: 40px;
+        height: 100%;
+        background: #f6f9fc;
+        top: 0;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid #eaeaea;
+        padding:8px;
+      }
+      
 
 
     &-primary {
