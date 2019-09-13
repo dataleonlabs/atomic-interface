ddafe440f646e21e5edf393a89dab3ec0c0cc296
Reema Samcom <reema@samcomtechnobrains.com>
2019-09-13 19:31:30 +0530

    fix: fixed the test case related issue

--- src/display/Well/index.test.tsx
+++ src/display/Well/index.test.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react'
 import * as enzyme from 'enzyme'
 import Well from './index'
-import { Info } from 'react-feather';
 
 describe('<Well />', () => {
     it('U-TEST-1 - Render Well', () => {
         const wrapper = enzyme.mount(
             <Well heading="Well">Demo Of Well</Well>
         )        
-        expect(wrapper.props()heading).toEqual(true);        
+        expect(wrapper.props().heading).toEqual('Well');        
     })
 
 })
