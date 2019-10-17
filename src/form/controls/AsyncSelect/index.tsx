c2728735e057c1325ce4b10e21777a808e6ef04f
Gérard TOKO <gerard@youngapp.co>
2019-10-17 07:27:31 +0200

    chore: async error

--- src/form/controls/AsyncSelect/index.tsx
+++ src/form/controls/AsyncSelect/index.tsx
@@ -11,45 +11,60 @@ import { FormControlHelper } from '../../formControlHelper';
  * Select render element
  */
 
-/* istanbul ignore next */
-const AsyncSelect = (props: Props) => {
+interface State {
+  onChanged: boolean
+}
 
-  const { name, ...rest } = props;
+/* istanbul ignore next */
+class AsyncSelect extends React.PureComponent<Props, State> {
+
+  public static defaultProps: Partial<Props> = {
+  }
+
+  public state: State = {
+    onChanged: false,
+  }
 
   /* istanbul ignore next */
-  const renderField = ({ field, form: { touched, values, submitCount, errors, setFieldValue } }: FieldProps<{}>) /* istanbul ignore next  */ => {
+  public renderField = ({ field, form: { touched, values, submitCount, errors, setFieldValue } }: FieldProps<{}>) /* istanbul ignore next  */ => {
+    const { name, ...rest } = this.props;
 
     /* istanbul ignore next  */
     const onChangeValue = (evt) => {
-      if (Array.isArray(evt)) {
-        let items: string[]; items = [];
-        evt.map((item) => {
-          items.push(item.value);
-        });
-        setFieldValue(name, items);
-      } else {
-        setFieldValue(name, evt.value);
-      }
+      this.setState({ onChanged: true }, () => {
+        if (Array.isArray(evt)) {
+          let items: string[]; items = [];
+          evt.map((item) => {
+            items.push(item.value);
+          });
+          setFieldValue(name, items);
+        } else {
+          if (evt) {
+            setFieldValue(name, evt.value);
+          } else {
+            setFieldValue(name, undefined);
+          }
+        }
+      });
     }
 
     /* istanbul ignore else */
     const objFormControlHelper = new FormControlHelper();
-    if (objFormControlHelper.checkConditional(props.conditionnals, values)) {
+    if (objFormControlHelper.checkConditional(this.props.conditionnals, values)) {
       return <></>;
     }
 
     let value = field.value;
-    
-    if (Object.keys(touched).length === 0) {
+    if ((Object.keys(touched).length === 0) && (this.state.onChanged === false)) {
       if (Array.isArray(values[name])) {
         let val: string[]; val = [];
         values[name].map((item) => {
-          value = (props.defaultOptions ? /* istanbul ignore next  */ props.defaultOptions.find(/* istanbul ignore next  */(option: any) => /* istanbul ignore next  */ option.value === item) : '') as any
+          value = (this.props.defaultOptions ? /* istanbul ignore next  */ this.props.defaultOptions.find(/* istanbul ignore next  */(option: any) => /* istanbul ignore next  */ option.value === item) : '') as any
           val.push(value);
         });
         value = val;
-      }else{
-        value = (props.defaultOptions ? /* istanbul ignore next  */ props.defaultOptions.find(/* istanbul ignore next  */(option: any) => /* istanbul ignore next  */ option.value === values[name]) : '') as any
+      } else {
+        value = (this.props.defaultOptions ? /* istanbul ignore next  */ this.props.defaultOptions.find(/* istanbul ignore next  */(option: any) => /* istanbul ignore next  */ option.value === values[name]) : '') as any
       }
     }
 
@@ -62,7 +77,7 @@ const AsyncSelect = (props: Props) => {
             {...field}
             value={value}
             {...rest}
-            isDisabled={props.readOnly}
+            isDisabled={this.props.readOnly}
             onChange={onChangeValue}
           />
           {/* istanbul ignore next  */ submitCount > 0 && /* istanbul ignore next  */ (errors["as_" + name] ? true : false)
@@ -72,14 +87,19 @@ const AsyncSelect = (props: Props) => {
       </Control>
     )
   }
-  return (
-    <Field
-      name={"as_" + name}
-      {...rest}
-      id={"as_" + name}
-      render={renderField}
-    />
-  )
+
+  public render() {
+    const { name, ...rest } = this.props;
+
+    return (
+      <Field
+        name={"as_" + name}
+        {...rest}
+        id={"as_" + name}
+        render={this.renderField}
+      />
+    )
+  }
 }
 
 /* istanbul ignore next */
